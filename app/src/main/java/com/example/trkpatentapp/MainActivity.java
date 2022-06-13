c4fd0351647c6612e9c69e087108aea5ef060afa
Ece Ateş <eceates625@gmail.com>
2022-06-13 12:17:09 +0300

    Basic Activities

--- app/src/main/java/com/example/trkpatentapp/MainActivity.java
+++ app/src/main/java/com/example/trkpatentapp/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.trkpatentapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationItemView nav = findViewById(R.id.bottom_na)
     }
 }
---
+++ app/src/main/java/com/example/trkpatentapp/bilgi_merkezi.java
@@ -0,0 +1,18 @@
+package com.example.trkpatentapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class bilgi_merkezi extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bilgi_merkezi, container, false);
+    }
+}
