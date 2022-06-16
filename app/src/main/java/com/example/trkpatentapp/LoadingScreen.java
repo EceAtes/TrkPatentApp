0ccbc146b0c2aeff9413951c4128414e75669972
Ece Ateş <eceates625@gmail.com>
2022-06-16 12:02:06 +0300

    Loading screen added

---
+++ app/src/main/java/com/example/trkpatentapp/LoadingScreen.java
@@ -0,0 +1,28 @@
+package com.example.trkpatentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LoadingScreen.this, MainActivity.class));
+                finish();
+            }
+        },2000);
+
+
+
+    }
+}
