cb6c62edc30e66d2120b1d1a902233e8da502c85
Ece Ateş <eceates625@gmail.com>
2022-06-28 12:58:22 +0300

    Main Page completed

--- app/src/main/java/com/example/trkpatentapp/MainActivity.java
+++ app/src/main/java/com/example/trkpatentapp/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView nav = findViewById(R.id.nav_view);
         nav.setSelectedItemId(R.id.navigation_home);
         if (Build.VERSION.SDK_INT >= 21) {
@@ -28,37 +33,121 @@ public class MainActivity extends AppCompatActivity {
         }
 
         nav.setOnNavigationItemSelectedListener(navListener);
-    }
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-        new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+        Button patentBasvuru = (Button) findViewById(R.id.başvuruPatent);
+        patentBasvuru.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selected = null;
-
-                switch(item.getItemId()){
-                    case R.id.navigation_home:
-                        selected = new giris();
-                        break;
-                    case R.id.navigation_kurumsal:
-                        selected = new kurumsal();
-                        break;
-                    case R.id.navigation_bilgi_merkezi:
-                    selected = new bilgi_merkezi();
-                    break;
-                    case R.id.navigation_hizmetler:
-                    selected = new hizmetler();
-                    break;
-                    case R.id.navigation_iletişim:
-                    selected = new fragment_iletisim();
-                    break;
-                }
-
-                if(selected != null) {
-                    getSupportFragmentManager().beginTransaction().replace(
-                            R.id.container, selected).commit();
-                }
-
-                return true;
+            public void onClick(View v) {
+                String url = "https://epats.turkpatent.gov.tr/run/TP/EDEVLET/giris";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
             }
-        };
+        });
+
+        Button patentArastirma = (Button) findViewById(R.id.araştırmaPatent);
+        patentArastirma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.turkpatent.gov.tr/arastirma-yap?form=patent";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+        Button basvuruMarka = (Button) findViewById(R.id.başvuruMarka);
+        basvuruMarka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://epats.turkpatent.gov.tr/run/TP/EDEVLET/giris";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+
+        Button arastirmaMarka = (Button) findViewById(R.id.araştırmaMarka);
+        arastirmaMarka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.turkpatent.gov.tr/arastirma-yap?form=trademark";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+
+        Button araştırmaTasarım = (Button) findViewById(R.id.araştırmaTasarım);
+        araştırmaTasarım.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.turkpatent.gov.tr/arastirma-yap?form=design";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+
+        Button başvuruTasarım = (Button) findViewById(R.id.başvuruTasarım);
+        başvuruTasarım.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://epats.turkpatent.gov.tr/run/TP/EDEVLET/giris";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+        Button başvuruİşaret = (Button) findViewById(R.id.başvuruİşaret);
+        başvuruİşaret.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://epats.turkpatent.gov.tr/run/TP/EDEVLET/giris";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+    }
+
+        private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selected = null;
+
+                        switch (item.getItemId()) {
+                            case R.id.navigation_home:
+                                selected = new giris();
+                                break;
+                            case R.id.navigation_kurumsal:
+                                selected = new kurumsal();
+                                break;
+                            case R.id.navigation_bilgi_merkezi:
+                                selected = new bilgi_merkezi();
+                                break;
+                            case R.id.navigation_hizmetler:
+                                selected = new hizmetler();
+                                break;
+                            case R.id.navigation_iletişim:
+                                selected = new fragment_iletisim();
+                                break;
+                        }
+
+                        if (selected != null) {
+                            getSupportFragmentManager().beginTransaction().replace(
+                                    R.id.container, selected).commit();
+                        }
+
+                        return true;
+                    }
+                };
+
+
 }
--- app/src/main/java/com/example/trkpatentapp/giris.java
+++ app/src/main/java/com/example/trkpatentapp/giris.java
@@ -1,9 +1,13 @@
 package com.example.trkpatentapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +17,88 @@ public class giris extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_iletisim, container, false);
+
+
+        Button patentBasvuru = (Button) view.findViewById(R.id.başvuruPatent);
+        patentBasvuru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://epats.turkpatent.gov.tr/run/TP/EDEVLET/giris";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+        Button patentArastirma = (Button) view.findViewById(R.id.araştırmaPatent);
+        patentArastirma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.turkpatent.gov.tr/arastirma-yap?form=patent";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+        Button basvuruMarka = (Button) view.findViewById(R.id.başvuruMarka);
+        basvuruMarka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://epats.turkpatent.gov.tr/run/TP/EDEVLET/giris";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+
+        Button arastirmaMarka = (Button) view.findViewById(R.id.araştırmaMarka);
+        arastirmaMarka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.turkpatent.gov.tr/arastirma-yap?form=trademark";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+
+        Button araştırmaTasarım = (Button) view.findViewById(R.id.araştırmaTasarım);
+        araştırmaTasarım.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.turkpatent.gov.tr/arastirma-yap?form=design";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+
+        Button başvuruTasarım = (Button) view.findViewById(R.id.başvuruTasarım);
+        başvuruTasarım.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://epats.turkpatent.gov.tr/run/TP/EDEVLET/giris";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+        Button başvuruİşaret = (Button) view.findViewById(R.id.başvuruİşaret);
+        başvuruİşaret.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://epats.turkpatent.gov.tr/run/TP/EDEVLET/giris";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
         return inflater.inflate(R.layout.fragment_giris, container, false);
     }
 }
