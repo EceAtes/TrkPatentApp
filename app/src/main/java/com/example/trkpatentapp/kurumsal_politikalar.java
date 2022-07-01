4380f9765e70a74a8c0ca2ee6c027b1661b7d3ac
Ece Ateş <eceates625@gmail.com>
2022-07-01 23:09:44 +0300

    Politikalar Activity completed

--- app/src/main/java/com/example/trkpatentapp/kurumsal_politikalar.java
+++ app/src/main/java/com/example/trkpatentapp/kurumsal_politikalar.java
@@ -2,7 +2,11 @@ package com.example.trkpatentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class kurumsal_politikalar extends AppCompatActivity {
 
@@ -10,5 +14,39 @@ public class kurumsal_politikalar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kurumsal_politikalar);
+
+
+        Button politika = (Button) findViewById(R.id.Politika);
+        politika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.turkpatent.gov.tr/kurumsal-politikalar";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+        Button kvkk = (Button) findViewById(R.id.KVKK);
+        politika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.turkpatent.gov.tr/kvkk";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
+
+        Button bilgigüv = (Button) findViewById(R.id.BilgiGüv);
+        bilgigüv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.turkpatent.gov.tr/27001-bilgi-guvenligi";
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
     }
 }
