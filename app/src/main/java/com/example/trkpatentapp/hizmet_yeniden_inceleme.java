be6474ae60f5fe7618b2998ae2abfd9e36c0b4be
Ece Ateş <eceates625@gmail.com>
2022-07-03 18:13:12 +0300

    Entegre Devre Topoğrafrası completed

---
+++ app/src/main/java/com/example/trkpatentapp/hizmet_yeniden_inceleme.java
@@ -0,0 +1,14 @@
+package com.example.trkpatentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hizmet_yeniden_inceleme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hizmet_yeniden_inceleme);
+    }
+}
